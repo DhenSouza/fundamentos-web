82dfe1d231f17dfe531acf8fe4359213dd5ab8f5
ShenSouza <ds052798ts@gmail.com>
2021-03-11 12:19:19 -0300

    feat: Inserido Funções de validação email e nome na página contato #4

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,28 @@ let email = document.querySelector("#email")
 let assunto = document.querySelector('#assunto')
 
 nome.style.width = "100%"
+email.style.width = "100%"
+
+
+function validaNome () {
+    let txtNome = document.querySelector("#txtNome")
+    if (nome.value.length < 3) {
+       txtNome.innerHTML = "Nome inválido"
+       txtNome.style.color = "red"
+    } else {
+        txtNome.innerHTML = "Nome Válido"
+        txtNome.style.color = "green"
+    }
+}
+
+function validaEmail() {
+    let txtEmail = document.querySelector("#txtEmail")
+    
+    if(email.value.indexOf("@") == -1 || email.value.indexOf(".") == -1) {
+        txtEmail.innerHTML = "E-mail Inválido"
+        txtEmail.style.color = "red"
+    } else {
+        txtEmail.innerHTML = "E-mail Valido"
+        txtEmail.style.color = "green"
+    }
+}
